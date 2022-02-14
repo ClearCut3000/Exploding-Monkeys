ae31daf29e23e395614c45a9e000e0a1d51c4c64
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-02-14 22:02:18 +0300

    Building the environment: SKTexture and filling a path

---
+++ Exploding Monkeys/CollisionTypes.swift
@@ -0,0 +1,14 @@
+//
+//  CollisionTypes.swift
+//  Exploding Monkeys
+//
+//  Created by Николай Никитин on 14.02.2022.
+//
+
+import Foundation
+
+enum CollisionTypes: UInt32 {
+  case banana = 1
+  case building = 2
+  case player = 4
+}
